f318c472a5c2090e3f5407bcba0d8ba94b50e267
WATA-Haru <43723360+WATA-Haru@users.noreply.github.com>
2026-02-23 19:18:27 +0000

    feat: dotfiles setup command !!

--- .devcontainer/nvimcontainer.sh
+++ .devcontainer/nvimcontainer.sh
@@ -5,27 +5,28 @@ if [[ "$1" == "-r" || "$1" == "--rebuild" ]]; then
   rebuild_flag="--remove-existing-container"
 fi
 
+# Dotfiles configuration (temporary, hard-coded)
+DOTFILES_REPO="https://github.com/WATA-Haru/dotfiles.git"
+DOTFILES_INSTALL="install/devcontainers/ubuntu/install.sh"
+DOTFILES_TARGET="/home/vscode/dotfiles"
+dotfiles_args="--dotfiles-repository $DOTFILES_REPO \
+  --dotfiles-install-command $DOTFILES_INSTALL \
+  --dotfiles-target-path $DOTFILES_TARGET"
+
 # Get the Neovim config path using headless nvim
 config_path=$(nvim --headless -c 'lua io.stdout:write(vim.fn.stdpath("config"))' -c 'q' --clean)
 
 # Resolve symlink for the config path
 resolved_config_path=$(readlink -f "$config_path")
 
-# dotfile config
-dotfiles-repository="https://github.com/WATA-Haru/dotfiles.git"
-dotfiles-target-path="~/dotfiles"
-dotfiles-install-command="~/dotfiles/install.sh"
-
 # Construct the command to run the devcontainer
+#    --additional-features='{ \
+#        \"ghcr.io/duduribeiro/devcontainer-features/neovim:1\": { \"version\": \"stable\" }, \
+#    }' \
 command="devcontainer up $rebuild_flag \
-    --mount type=bind,source=$resolved_config_path,target=/home/code/.config/container-nvim \
-    --additional-features='{ \
-        \"ghcr.io/duduribeiro/devcontainer-features/neovim:1\": { \"version\": \"stable\" }, \
-    }' \
-    --dotfiles-repository=$dotfiles-repository \
-    --dotfiles-target-path=$dotfiles-target-path \
-    --dotfiles-install-command=$dotfiles-install-command \
+    $dotfiles_args \
+    --mount type=bind,source=$resolved_config_path,target=/home/vscode/.config/nvim \
     --workspace-folder ."
 
 eval "$command"
-eval "devcontainer exec --remote-env NVIM_APPNAME=container-nvim --workspace-folder . nvim"
+eval "devcontainer exec --workspace-folder . bash"
