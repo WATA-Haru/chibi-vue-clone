5e525ed77413ff2ddc82178f4c801bc420cb729f
WATA-Haru <43723360+WATA-Haru@users.noreply.github.com>
2026-02-28 20:52:26 +0900

    fix: devcontainer --network host

--- .devcontainer/nvimcontainer.sh
+++ .devcontainer/nvimcontainer.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-rebuild_flag=""
-if [[ "$1" == "-r" || "$1" == "--rebuild" ]]; then
-  rebuild_flag="--remove-existing-container"
-fi
-
 # Dotfiles configuration (temporary, hard-coded)
 DOTFILES_REPO="https://github.com/WATA-Haru/dotfiles.git"
 DOTFILES_INSTALL="install/devcontainers/ubuntu/install.sh"
@@ -13,22 +8,11 @@ dotfiles_args="--dotfiles-repository $DOTFILES_REPO \
   --dotfiles-install-command $DOTFILES_INSTALL \
   --dotfiles-target-path $DOTFILES_TARGET"
 
-# Get the Neovim config path using headless nvim
-config_path=$(nvim --headless -c 'lua io.stdout:write(vim.fn.stdpath("config"))' -c 'q' --clean)
 
-# Resolve symlink for the config path
-resolved_config_path=$(readlink -f "$config_path")
-
-# Construct the command to run the devcontainer
-#    --additional-features='{ \
-#        \"ghcr.io/duduribeiro/devcontainer-features/neovim:1\": { \"version\": \"stable\" }, \
-#    }' \
-command="devcontainer up $rebuild_flag \
-    $dotfiles_args \
-    --mount type=bind,source=$resolved_config_path,target=/home/vscode/.config/nvim \
-    --workspace-folder ."
-
-eval "$command"
+eval "devcontainer up  $dotfiles_args \
+      --config .devcontainer/my-override/devcontainer.json \
+      --workspace-folder ."
 eval "devcontainer exec \
+      --config .devcontainer/my-override/devcontainer.json \
       --remote-env REMOTE_CONTAINERS=true \
       --workspace-folder . bash"
